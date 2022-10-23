69add090ce4b27d0fbcefa42f1738c0c322ab6f8
Ohad Shivek <oshivek@outbrain.com>
2022-10-23 21:02:27 +0300

    basic grid and camera control

--- index.js
+++ index.js
@@ -3,107 +3,60 @@ import {
     OrbitControls
 } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js';
 
-import {
-    GLTFLoader
-} from 'https://unpkg.com/three@0.139.2/examples/jsm/loaders/GLTFLoader.js';
-
-let model, sun;
-
-function createRenderer() {
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.toneMapping = THREE.ReinhardToneMapping;
-    renderer.toneMappingExposure = 3;
-    renderer.shadowMap.enabled = true;
-    return renderer;
-}
-
-function createCamera() {
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.z = 5;
-    return camera;
-}
-
-function addModel(scene) {
-    const loader = new GLTFLoader();
-    loader.load('assets/little_hermit_crab/scene.gltf', result => {
-        model = result.scene.children[0];
-        model.traverse(obj => {
-            if (obj.isMesh) {
-                obj.castShadow = true;
-                obj.receiveShadow = true;
-
-                if (obj.material.map) {
-                    obj.material.map.anisotrophy = 16;
-                }
-            }
-        });
-        scene.add(model);
-
-
-        // Ground plane
-        const geo = new THREE.PlaneGeometry(6, 6, 8, 8);
-        const mat = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide });
-        const plane = new THREE.Mesh(geo, mat);
-        plane.rotateX(- Math.PI / 2);
-        plane.position.y -= 0.5;
-        plane.receiveShadow = true;
-        scene.add(plane);
-    });
-}
-
-function addCubemap(scene) {
-    const path = "https://threejs.org/examples/textures/cube/skyboxsun25deg/";
-    const format = '.jpg';
-    const urls = [
-        `${path}px.jpg`,
-        `${path}nx.jpg`,
-        `${path}py.jpg`,
-        `${path}ny.jpg`,
-        `${path}pz.jpg`,
-        `${path}nz.jpg`,
-    ];
-    const loader = new THREE.CubeTextureLoader();
-    const cubeMap = loader.load(urls);
-    cubeMap.format = THREE.RGBFormat;
-
-    scene.background = cubeMap;
-}
-function addLights(scene) {
-    const skyCol = new THREE.Color(0.513, 0.835, 0.866);
-    const groundCol = new THREE.Color(0.815, 0.184, 0.439);
-    const hemiLight = new THREE.HemisphereLight(skyCol, groundCol, 2);
-    scene.add(hemiLight);
-
-    sun = new THREE.SpotLight(0xffffff, 2.2);
-    sun.castShadow = true;
-    sun.shadow.bias = -0.0001;
-    sun.shadow.mapSize = new THREE.Vector2(4096, 4096);
-    scene.add(sun);
-}
-
 export function main() {
-    const scene = new THREE.Scene();
-    const camera = createCamera();
-    const renderer = createRenderer();
-    const controls = new OrbitControls(camera, renderer.domElement);
-    document.body.appendChild(renderer.domElement);
 
-    addCubemap(scene);
-    addLights(scene);
-    addModel(scene);
+      const GAP = 1;
+		
+				const scene = new THREE.Scene();
+			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			camera.position.set(0,10,10);
+			camera.lookAt( 0, 0, 0 );
 
-    function render(time) {
-        time *= 0.001; // convert time to seconds
 
-        sun.position.set(
-            camera.position.x + 5,
-            camera.position.y + 20,
-            camera.position.z + 10,
-        );
+			const renderer = new THREE.WebGLRenderer({antialias:true});
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( renderer.domElement );
 
-        renderer.render(scene, camera);
-        requestAnimationFrame(render);
-    }
-    requestAnimationFrame(render);
+			const controls = new OrbitControls( camera, renderer.domElement );
+			controls.update();
+
+			const cells = [
+				[{height: 1},{height: 0},{height:2}],
+				[{height: 4},{height: 1},{height:5}],
+				[{height: 1},{height: 0},{height:0}]
+			];
+			const cubes = [];
+
+								console.log('init');
+
+			for(let z=0; z<cells.length; z++) {
+				for(let x=0 ; x<cells[0].length; x++) {
+									console.log('init');
+
+					const cell = cells[z][x];
+					console.log(cell);
+					const material = new THREE.MeshBasicMaterial( {color: 0x00ff00, wireframe: true}  );
+					const geometry = new THREE.BoxGeometry( 0.9, cell.height, 0.9 );
+					const cube = new THREE.Mesh( geometry, material );
+					cube.position.set(x,(cell.height/2),z);
+					scene.add( cube );
+					cubes.push(cube);
+			}
+		}
+
+
+
+			function animate() {
+				requestAnimationFrame( animate );
+				
+				// cubes.map((cube)=> {
+				// 	cube.rotation.y += 0.01;
+					
+				// })
+				
+				// controls.update();
+				renderer.render( scene, camera );
+			}
+			animate();
 }
+	
